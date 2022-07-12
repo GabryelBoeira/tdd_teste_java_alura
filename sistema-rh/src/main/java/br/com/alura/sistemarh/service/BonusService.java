2314daf0b3a0e011c8902effe00e83692282ac52
gabryelboeira <gabryel392@gmail.com>
2022-07-11 22:49:46 -0300

    Projeto pronto da alura para ser utilizado para criacao dos teste unitarios

---
+++ sistema-rh/src/main/java/br/com/alura/sistemarh/service/BonusService.java
@@ -0,0 +1,17 @@
+package br.com.alura.sistemarh.service;
+
+import java.math.BigDecimal;
+
+import br.com.alura.sistemarh.modelo.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+			valor = BigDecimal.ZERO;
+		}
+		return valor;
+	}
+
+}
