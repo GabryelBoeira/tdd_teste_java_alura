edebface48c3dfc86e9e7163905b0e4e19424b83
gabryelboeira <gabryel392@gmail.com>
2022-07-17 00:42:20 -0300

    Aplicando refatoracao do codigo com segura por existir teste garantindo o funcionamento base do calculo do reajuste

--- sistema-rh/src/main/java/br/com/alura/sistemarh/enums/DesempenhoFuncionario.java
+++ sistema-rh/src/main/java/br/com/alura/sistemarh/enums/DesempenhoFuncionario.java
@@ -1,17 +1,25 @@
 package br.com.alura.sistemarh.enums;
 
+import java.math.BigDecimal;
+
 public enum DesempenhoFuncionario {
-    A_DESEJAR("A Desejar"),
-    BOM("Bom"),
-    OTIMO("Otimo");
+    A_DESEJAR("A Desejar", new BigDecimal("0.03")),
+    BOM("Bom", new BigDecimal("0.15")),
+    OTIMO("Otimo", new BigDecimal("0.2"));
 
     private String nome;
+    private BigDecimal valorReajuste;
 
-    DesempenhoFuncionario(String nome) {
+    DesempenhoFuncionario(String nome, BigDecimal valorReajuste) {
         this.nome = nome;
+        this.valorReajuste = valorReajuste;
     }
 
     public String getNome(){
         return this.nome;
+    }
+
+    public BigDecimal getValorReajuste(){
+        return this.valorReajuste;
     }
 }
