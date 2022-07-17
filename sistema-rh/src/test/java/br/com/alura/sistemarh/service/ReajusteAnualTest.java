0d7c0c63694d727982de22c194a84eef36d5a726
gabryelboeira <gabryel392@gmail.com>
2022-07-17 01:20:36 -0300

    Como organizar o código da classe de teste

--- sistema-rh/src/test/java/br/com/alura/sistemarh/service/ReajusteAnualTest.java
+++ sistema-rh/src/test/java/br/com/alura/sistemarh/service/ReajusteAnualTest.java
@@ -2,6 +2,7 @@ package br.com.alura.sistemarh.service;
 
 import br.com.alura.sistemarh.enums.DesempenhoFuncionario;
 import br.com.alura.sistemarh.modelo.Funcionario;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,11 +13,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReajusteAnualTest {
 
+    private ReajusteAnualService reajusteService;
+    private Funcionario funcionario;
+
+    @BeforeEach
+    public void inicializar() {
+        this.reajusteService = new ReajusteAnualService();
+        this.funcionario = new Funcionario("Ana", LocalDate.now(), new BigDecimal("2500"));
+    }
+
     @Test
     @DisplayName("Desempenho foi `A desejar`: Reajuste aplicavel sera de 3% em relacao ao salario")
     void reajusteAnualDesempenhoADesejar() {
-        ReajusteAnualService reajusteService = new ReajusteAnualService();
-        BigDecimal valorReajuste = reajusteService.calcularReajuste(DesempenhoFuncionario.A_DESEJAR, new Funcionario("Teste 1", LocalDate.now(), new BigDecimal("2500")));
+        BigDecimal valorReajuste = reajusteService.calcularReajuste(DesempenhoFuncionario.A_DESEJAR, funcionario);
 
         assertEquals(new BigDecimal("75.00"), valorReajuste);
     }
@@ -24,8 +33,7 @@ public class ReajusteAnualTest {
     @Test
     @DisplayName("Desempenho foi `Bom`: Reajuste aplicavel sera de 15% em relacao ao salario")
     void reajusteAnualDesempenhoBom() {
-        ReajusteAnualService reajusteService = new ReajusteAnualService();
-        BigDecimal valorReajuste = reajusteService.calcularReajuste(DesempenhoFuncionario.BOM, new Funcionario("Teste 2", LocalDate.now(), new BigDecimal("2500")));
+        BigDecimal valorReajuste = reajusteService.calcularReajuste(DesempenhoFuncionario.BOM, funcionario);
 
         assertEquals(new BigDecimal("375.00"), valorReajuste);
     }
@@ -33,8 +41,7 @@ public class ReajusteAnualTest {
     @Test
     @DisplayName("Desempenho foi `Otimo`: Reajuste aplicavel sera de 20% em relacao ao salario")
     void reajusteAnualDesempenhoOtimo() {
-        ReajusteAnualService reajusteService = new ReajusteAnualService();
-        BigDecimal valorReajuste = reajusteService.calcularReajuste(DesempenhoFuncionario.OTIMO, new Funcionario("Teste 3", LocalDate.now(), new BigDecimal("2500")));
+        BigDecimal valorReajuste = reajusteService.calcularReajuste(DesempenhoFuncionario.OTIMO, funcionario);
 
         assertEquals(new BigDecimal("500.00"), valorReajuste);
     }
