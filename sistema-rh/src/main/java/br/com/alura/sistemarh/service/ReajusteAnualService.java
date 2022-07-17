2d2df900c3c4f3b75d4936b2119aaa5cb49112e7
gabryelboeira <gabryel392@gmail.com>
2022-07-17 00:42:05 -0300

    Aplicando refatoracao do codigo com segura por existir teste garantindo o funcionamento base do calculo do reajuste

--- sistema-rh/src/main/java/br/com/alura/sistemarh/service/ReajusteAnualService.java
+++ sistema-rh/src/main/java/br/com/alura/sistemarh/service/ReajusteAnualService.java
@@ -7,22 +7,10 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class ReajusteAnualService {
+
     public BigDecimal calcularReajuste(DesempenhoFuncionario desempenho, Funcionario funcionario) {
-        BigDecimal reajuste;
-        switch (desempenho) {
-            case A_DESEJAR:
-                reajuste = new BigDecimal("0.03").multiply(funcionario.getSalario());
-                break;
-            case BOM:
-                reajuste = new BigDecimal("0.15").multiply(funcionario.getSalario());
-                break;
-            case OTIMO:
-                reajuste = new BigDecimal("0.2").multiply(funcionario.getSalario());
-                break;
-            default:
-                reajuste = BigDecimal.ZERO;
-                break;
-        }
-        return reajuste.setScale(2, RoundingMode.HALF_UP);
+        return desempenho.getValorReajuste().multiply(funcionario.getSalario())
+                .setScale(2, RoundingMode.HALF_UP);
     }
+
 }
